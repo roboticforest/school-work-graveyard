ad7e69735e8535d5dffb755c6bb0473b15ab5980
roboticforest <roboticforest@gmail.com>
2021-04-10 20:42:44 -0700

    Completed docstrings and filled out basic init and getter functions.

--- AdvRoom.py
+++ AdvRoom.py
@@ -11,6 +11,7 @@ This module is responsible for modeling a single room in Adventure.
 """
 
 import typing
+
 from text_utils import ordinal_str
 
 ###########################################################################
@@ -30,20 +31,42 @@ MARKER = "-----"
 
 class AdvRoom:
 
-    def __init__(self, name, shortdesc, longdesc, passages):
-        """Creates a new room with the specified attributes."""
+    def __init__(self, name: str, short_desc: str, long_desc: list, room_exits: dict):
+        """
+        Initialized an Adventure Room object with the specified attributes.
 
-    def get_name(self):
-        """Returns the name of this room.."""
+        :param name: The unique ID of the room. This should be a human readable plain text string, like "EndOfRoad".
+        :param short_desc: A single line description of the room to display to the player.
+        :param long_desc: A list of lines of text to display to the user which describes the appearance of the room they
+         are standing within.
+        :param room_exits: A dictionary of room exits. Expected is a dict[str, str], where the key is a string representing
+         the directional command players will type and the data will be the ID of the room to send the player to.
+        """
+        self._name = name
+        self._short_description = short_desc
+        self._long_description = long_desc
+        self._exits = room_exits
 
-    def get_short_description(self):
-        """Returns a one-line short description of this room.."""
+    def get_name(self) -> str:
+        """Returns the name of this room."""
+        return self._name
 
-    def get_long_description(self):
+    def get_short_description(self) -> str:
+        """Returns a one-line short description of this room."""
+        return self._short_description
+
+    def get_long_description(self) -> list:
         """Returns the list of lines describing this room."""
+        return self._long_description
 
-    def get_next_room(self, verb):
-        """Returns the name of the destination room after applying verb."""
+    def get_next_room(self, travel_command: str):
+        """
+        Tries to look up the name of a neighboring room associated with the given travel command word
+         (such as "NORTH", or "IN").
+        :param travel_command: A string containing, typically, a travel direction such as "SOUTH" or "OUT".
+        :return: The ID of the neighboring connected room, if there is one connected via the given command word.
+        """
+        
 
     @staticmethod
     def read_room(room_file: typing.TextIO):
@@ -94,7 +117,7 @@ class AdvRoom:
         # Read in the list of available room changing verbs and destinations.
         # The list end is marked by a blank line.
         done = False
-        passages = {}
+        room_exits = {}
         while not done:
             line = room_file.readline().strip()
             if line == "":
@@ -105,8 +128,8 @@ class AdvRoom:
                     msg = "Missing colon separator on the {0} line of room {1}. The line reads: \"{2}\"".format(
                         ordinal_str(lines_read), name, line)
                     raise ValueError(msg)
-                room_exit_verb = line[:split_pos].upper()
+                exit_command = line[:split_pos].upper()
                 destination_room = line[split_pos + 1:].strip()
-                passages[room_exit_verb] = destination_room
+                room_exits[exit_command] = destination_room
 
-        return AdvRoom(name, short_desc, long_desc, passages)
+        return AdvRoom(name, short_desc, long_desc, room_exits)
