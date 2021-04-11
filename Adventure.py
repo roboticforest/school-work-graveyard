542e048f2639df4a356432b722f4b4ce072c57ed
roboticforest <roboticforest@gmail.com>
2021-04-10 20:14:22 -0700

    Restructured to better match the design of the TeachingMachine template and filled with similar code.

--- Adventure.py
+++ Adventure.py
@@ -5,16 +5,35 @@
 from AdvGame import AdvGame
 
 # Constants
-
 DATA_FILE_PREFIX = "Tiny"
 
-# Main program
 
+# Main program
 def adventure():
-    game = AdvGame(DATA_FILE_PREFIX)
+    """
+    Main Program. Loads and runs an Adventure Game.
+
+    :return: Nothing.
+    """
+    game = load_adventure_game()
     game.run()
 
-# Startup code
 
+def load_adventure_game():
+    """
+    Loads all Room, Object, and Synonym text files specified by DATA_FILE_PREFIX.
+
+    :return: A new Adventure Game instance.
+    """
+    try:
+        adventure_file = DATA_FILE_PREFIX + "Rooms.txt"
+        with open(adventure_file) as room_file:
+            return AdvGame.read_adventure(room_file)
+    except IOError as err:
+        print("Could not load the Adventure Game from file:", adventure_file)
+        print(err)
+
+
+# Startup code
 if __name__ == "__main__":
     adventure()
